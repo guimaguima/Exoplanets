3c7c7e2bad9cacdfac89c5a5efd1e34bd5688571
EnzoBraz <enzodesouzabraz2004@gmail.com>
2026-06-08 10:22:51 -0300

    [REFACTOR(DATA)] melhorando a engenharia de features para a modelagem

--- bayes_model.R
+++ bayes_model.R
@@ -3,7 +3,7 @@
 #install.packages("tidymodels")
 library(tidyverse)
 library(tidymodels)
-library(rstanarm) 
+library(rstanarm)
 library(bayesplot)
 
 planetas_raw <- read_csv('bayes/cumulative.csv')
@@ -12,9 +12,7 @@ colunas_para_remover <- c(
   'rowid', 'kepid', 'kepoi_name', 'kepler_name',
   'koi_pdisposition', 'koi_score',
   'koi_fpflag_nt', 'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec',
-  'koi_teq_err1', 'koi_teq_err2',
-  'koi_tce_delivname',
-  'koi_period_err2', 'koi_time0bk_err2', 'koi_depth_err2', 'koi_duration_err2'
+  'koi_tce_delivname'
 )
 
 eps <- 1e-8
@@ -22,7 +20,7 @@ eps <- 1e-8
 planetas <- planetas_raw %>%
   select(-any_of(colunas_para_remover)) %>%
   filter(koi_disposition %in% c("CONFIRMED", "FALSE POSITIVE")) %>%
-  drop_na() %>%
+  drop_na(koi_steff, koi_slogg, koi_srad, koi_period, koi_depth, koi_prad, koi_model_snr) %>%
   mutate(
     koi_disposition = as.factor(koi_disposition),
     koi_period_rel_err = koi_period_err1 / (abs(koi_period) + eps),
@@ -31,14 +29,26 @@ planetas <- planetas_raw %>%
     koi_depth_snr_weighted = koi_depth * koi_model_snr
   )
 
+dados_pca <- planetas %>% select(koi_steff, koi_slogg, koi_srad)
+pca_estelar <- prcomp(dados_pca, center = TRUE, scale. = TRUE)
+planetas$stellar_pca_index <- pca_estelar$x[, 1]
+
+preditores <- c('koi_period', 'koi_period_err1', 'koi_time0bk', 'koi_time0bk_err1',
+       'koi_impact', 'koi_impact_err1', 'koi_impact_err2', 'koi_duration',
+       'koi_duration_err1', 'koi_depth', 'koi_depth_err1', 'koi_prad',
+       'koi_prad_err1', 'koi_prad_err2', 'koi_teq', 'koi_insol',
+       'koi_insol_err1', 'koi_insol_err2', 'koi_model_snr', 'koi_tce_plnt_num',
+       'koi_steff_err1', 'koi_steff_err2', 'koi_slogg_err1', 'koi_slogg_err2',
+       'koi_srad_err1', 'koi_srad_err2', 'ra', 'dec', 'koi_kepmag',
+       'koi_period_rel_err', 'koi_depth_rel_err', 'koi_prad_rel_err',
+       'koi_depth_snr_weighted', 'stellar_pca_index')
+
+planetas <- planetas %>% drop_na(any_of(preditores))
+
 set.seed(42)
 split <- initial_split(planetas, prop = 0.70, strata = koi_disposition)
 planetas_treino <- training(split)
 planetas_teste  <- testing(split)
-
-preditores <- c("koi_period", "koi_depth", "koi_teq", 
-                "koi_period_rel_err", "koi_depth_rel_err", 
-                "koi_prad_rel_err", "koi_depth_snr_weighted")
 
 planetas_treino <- planetas_treino %>%
   mutate(across(all_of(preditores), ~ as.numeric(scale(.))))
